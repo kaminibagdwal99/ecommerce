82ff275b92d8e827884ef9fae22d46ad13e5dba3
kaminibagdwal99 <kaminibagdwal16@gmail.com>
2024-08-20 16:32:43 +0530

    changes

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kamini99.bagdwal@gmail.com'
-EMAIL_HOST_PASSWORD ="zofz gzgb aalc oyia"
+EMAIL_HOST_USER = '*******@gmail.com'
+EMAIL_HOST_PASSWORD ="**********"
 EMAIL_USE_TLS = True
